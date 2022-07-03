7bef2a4d10268bed32d7c7383175578ff92dfc17
Christopher Centrella <ccentrella.j@gmail.com>
2022-07-03 18:45:05 -0400

    Update homepage
    Update homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,25 @@ Rails.application.routes.draw do
 
   # Define additional paths
   get '/about', to: "static_pages#about"
+  get '/resume', to: "resources#resume"
   get '/mission', to: "static_pages#mission"
   get '/contact', to: "email_contact#new"
   post "/contact_form", to: "email_contact#create", as: "email_contacts"
   get '/blog', to: "posts#index"
   get '/photos', to: "photos#index"
 
+  # Define external URLs
+  direct :github do
+    "https://github.com/ccentrella"
+  end
+
+  direct :linkedin do
+    "https://linkedin.com/in/ccentrella/"
+  end
+
   # Required for contact form
   resources :email_contact, only: [:new, :create]
+
+  resources :resources, path: '/resources'
 
 end
