a4f44f6441f2da846d7d0dee453f6c492e70cab1
Christopher Centrella <ccentrella.j@gmail.com>
2022-07-30 20:53:27 -0400

    Added calculate read time feature for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,23 @@ class Post < ApplicationRecord
     has_rich_text :content
 
     def get_time()
-        return '5 mins.'
+        words = content.to_plain_text().split()
+        minutes = round([1, words.length / 200].max)
+        message = "%d minute".pluralize(minutes) % minutes
+        return message
     end
 
     def get_author()
         return User.get_user_name(user_id)
+    end
+
+    # Round to nearest five minutes
+    def round(minutes)
+        if minutes < 5
+            return minutes
+        else
+            remainder = minutes % 5
+            return remainder < 3 ? minutes - remainder : minutes + 5 - remainder
+        end
     end
 end
