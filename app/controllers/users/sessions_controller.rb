aa86ab2e395cccfc42b3fea7066b07a31c61c5aa
Christopher Centrella <ccentrella.j@gmail.com>
2022-08-01 20:24:01 -0400

    Temporary workaround to enable messages and sign_in redirect for Devise.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,6 +6,15 @@ class Users::SessionsController < Devise::SessionsController
   def home
   end
 
+  def after_sign_in_path_for(resource)
+    user_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
