2703025c0a9e11a25af361deaa2aa666bb334264
Christopher Centrella <ccentrella.j@gmail.com>
2022-08-22 18:32:25 -0400

    Pretty file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,9 @@ class ApplicationController < ActionController::Base
     
     def after_sign_in_path_for(resource)
        profile_path
-      end
+    end
     
-      def after_sign_out_path_for(resource)
-        root_path
-      end
-    
+    def after_sign_out_path_for(resource)
+      root_path
+    end
 end
