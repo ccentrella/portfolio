760f00344faf429d9b7d1b791ef4e61a56606e38
Christopher Centrella <ccentrella.j@gmail.com>
2022-10-10 19:51:53 -0400

    Working version of redesigned header

--- app/javascript/components/template-parts/Header.jsx
+++ app/javascript/components/template-parts/Header.jsx
@@ -1,38 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import MenuIcon from "../../../assets/images/icons/menu.svg";
 
 function Header() {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/resume">Resume</Link>
-                    </li>
-                    <li>
-                        <Link to="/interests">Interests</Link>
-                    </li>
-                    <li>
-                        <Link to="/specialties">Specialties</Link>
-                    </li>
-                    <li>
-                        <Link to="/highlights">Highlights</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact Me</Link>
-                    </li>
-
-                </ul>
-            </nav>
+        <>
+            <header>
+                <img
+                    src="/assets/favicon.png"
+                    alt="profile image"
+                    className="logo-image"
+                />
+                <h1 class="logo">
+                    <NavLink to="/">Chris Centrella</NavLink>
+                </h1>
+                <label className="menu-toggle" for="toggle">
+                    <img
+                        src={MenuIcon}
+                        className="menu-toggle"
+                        alt="menu icon"
+                    />
+                </label>
+                <input type="checkbox" id="toggle" />
+                <nav className="navbar">
+                    <ul>
+                        <li className="nav-item">
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/resume">Resume</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/highlights">Highlights</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/specialties">Specialties</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/interests">Interests</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/blog">Blog</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                target="_blank"
+                                href="https://lightroom.adobe.com/gallery/35c62e7ff3ce476f9ae5b07994eb6f15">
+                                Photo Gallery
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/contact">Contact Me</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
             <div className="navbar-sticky">
                 <h1 className="logo">
-                    <a href="/">CC</a>
+                    <Link to="/">CC</Link>
                 </h1>
             </div>
-        </header>
+        </>
     );
 }
 
