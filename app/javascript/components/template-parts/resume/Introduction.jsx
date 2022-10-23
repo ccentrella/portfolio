bd4d8f9246a37d2d093c2f696e8fb3edaf740d8a
Christopher Centrella <ccentrella.j@gmail.com>
2022-10-23 18:25:46 -0400

    Tweak resume button style and remove added button style

--- app/javascript/components/template-parts/resume/Introduction.jsx
+++ app/javascript/components/template-parts/resume/Introduction.jsx
@@ -10,15 +10,17 @@ function Introduction() {
             <p className="section-title zero-margin">Junior Software Engineer | Software Developer</p>
             <div className="container">
                 <p className="resume-location">Location: Lansing, MI</p>
-                <Link className="button-fixed button-thin" to="/">
-                    Homepage
-                </Link>
-                <a className="button-fixed button-thin" target="_blank" href="https://github.com/ccentrella">
-                    View GitHub
-                </a>
-                <a className="button-fixed button-thin" target="_blank" href="https://linkedin.com/in/ccentrella">
-                    View LinkedIn
-                </a>
+                <div className="resume-buttons">
+                    <Link className="button-fixed" to="/">
+                        Homepage
+                    </Link>
+                    <a className="button-fixed" target="_blank" href="https://github.com/ccentrella">
+                        View GitHub
+                    </a>
+                    <a className="button-fixed" target="_blank" href="https://linkedin.com/in/ccentrella">
+                        View LinkedIn
+                    </a>
+                </div>
             </div>
         </div>
     );
