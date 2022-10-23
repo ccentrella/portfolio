40a04fb6214076315c5cc1d28f69a5ea6b2d74c0
Christopher Centrella <ccentrella.j@gmail.com>
2022-10-23 16:55:05 -0400

    Extract content to use components instead, and update component import statements

--- app/javascript/components/pages/Interests.jsx
+++ app/javascript/components/pages/Interests.jsx
@@ -1,49 +1,16 @@
 import React from "react";
 
-import BlogBlock from "../template-parts/blocks/BlogBlock"
-import PhotosBlock from "../template-parts/blocks/PhotosBlock";
+import BlogBlock from "../template-parts/blocks/link-blocks/BlogLinkBlock";
+import GuitarBlogSectionBlocks from "../template-parts/blocks/section-blocks/GuitarBlogSectionBlocks";
+import PhotographySectionBlock from "../template-parts/blocks/section-blocks/PhotographySectionBlock";
 
 function Interests() {
     return (
         <>
-            <div className="container">
-                <p className="section-title">Interests</p>
-                <p className="large">
-                    Outside of writing code or designing user experiences, some
-                    of my favorite activities are blogging, photography, and
-                    playing guitar.
-                </p>
-                <h2>Personal Blog</h2>
-                <p className="large">
-                    My faith life is important to me and I love to write about
-                    how to find God amid the twists and turns of life, and so
-                    others might know more about Him.
-                </p>
-            </div>
+            <p className="section-title zero-margin">Interests</p>
+            <GuitarBlogSectionBlocks />
             <BlogBlock />
-            <div className="container">
-                <h2>Photography</h2>
-                <p className="large">
-                    Taking photos has been one of my favorite hobbies as early
-                    as my childhood. In a world full of pain and division, my
-                    mind is drawn to capture what’s truly beautiful. I
-                    especially love to take pictures of nature, because there is
-                    a sacredness and a beauty in the environment, unparalleled
-                    by human invention. For “the heavens proclaim the glory of
-                    God, and the firmament proclaims His handiwork.” (Psalm
-                    19:1).
-                </p>
-            </div>
-            <PhotosBlock />
-            <div className="container">
-                <h2>Guitar</h2>
-                <p className="large">
-                    The past couple years, I’ve been trying to pick up playing
-                    guitar, especially for worship. Picking up how to play a
-                    musical instrument can be a lot of work, but I’m SLOWLY
-                    getting there with a lot of practice.
-                </p>
-            </div>
+            <PhotographySectionBlock />
         </>
     );
 }
