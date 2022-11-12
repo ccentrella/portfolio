3e87958626c929d7b4209ef142812046bed52ea2
Christopher Centrella <ccentrella.j@gmail.com>
2022-11-12 14:01:16 -0500

    Update blog block to show latest article

--- app/javascript/components/template-parts/blocks/link-blocks/BlogLinkBlock.jsx
+++ app/javascript/components/template-parts/blocks/link-blocks/BlogLinkBlock.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LinkBlock from "./LinkBlock";
 import BlogIcon from "../../../../../assets/images/icons/article.svg";
 
 function BlogLinkBlock() {
+    const [latestPost, setLatestPost] = useState({ value: 0, title: "", link: "" });
+
+    useEffect(() => {
+        fetch("/api/v1/blog/latest_article")
+            .then((response) => response.json())
+            .then(
+                (post) => {
+                    console.log(post);
+                    setLatestPost({ ...latestPost, title: post.title, link: "/blog/" + post.slug });
+                },
+            );
+    }, [latestPost.value]);
+
     return (
         <LinkBlock
-            title="{Latest Blog Post}"
+            title={latestPost.title}
             icon={BlogIcon}
             links={[
-                [
-                    (linkDescription = "View Article"),
-                    (linkPath = "/blog"),
-                    (isExternal = false),
-                ],
-                [
-                    (linkDescription = "Blog Homepage"),
-                    (linkPath = "/blog"),
-                    (isExternal = false),
-                ],
+                [(linkDescription = "View Article"), (linkPath = latestPost.link), (isExternal = false)],
+                [(linkDescription = "Blog Homepage"), (linkPath = "/blog"), (isExternal = false)],
             ]}
         />
     );
