b2dc2926ad7dc891f461406ac1e7a92e030c36ee
Christopher Centrella <ccentrella.j@gmail.com>
2022-11-16 19:45:58 -0500

    Update contact view and work on contact React component

--- app/javascript/components/pages/Contact.jsx
+++ app/javascript/components/pages/Contact.jsx
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Contact() {
-    return (
-        <div className="container">
-            <h1>Contact</h1>
-            <p>
-                Thank you for visiting my website. You can reach out to me via
-                the form below.
-            </p>
-            <p>
-                If you prefer to contact me via LinkedIn, you can use this link:
-            </p>
+    useEffect(() => {
+        <script src="https://www.google.com/recaptcha/api.js"></script>;
 
-            <div className="button-fixed-container">
-                <a
-                    className="button-fixed"
-                    target="_blank"
-                    href="https://linkedin.com/in/ccentrella">
-                    LinkedIn Profile
-                </a>
-                <Link className="button-fixed" to="/">
-                    View Homepage
-                </Link>
+        function onSubmit(token) {
+            document.getElementById("demo-form").submit();
+        }
+    }, []);
+
+    return (
+        <>
+            <div className="container">
+                <h1>Contact</h1>
+                <p>Thank you for visiting my website. You can reach out to me via the form below.</p>
+                <p>If you prefer to contact me via LinkedIn, you can use this link:</p>
+
+                <div className="button-fixed-container">
+                    <a className="button-fixed" target="_blank" href="https://linkedin.com/in/ccentrella">
+                        LinkedIn Profile
+                    </a>
+                    <Link className="button-fixed" to="/">
+                        View Homepage
+                    </Link>
+                </div>
             </div>
-        </div>
+
+            {/* <%= render partial: "shared/error_messages", object: @email_contact %> */}
+            <form method="post" action="/contact" className="contact-form">
+                <input type="text" name="name" placeholder="Name" className="field" />
+                <br />
+                <input type="email" name="email" placeholder="Name" className="field" />
+                <br />
+                <textarea name="message" placeholder="Message" className="field field-message" />
+                <br />
+                <button className="g-recaptcha field submit-button" data-sitekey="reCAPTCHA_site_key" data-callback="onSubmit" data-action="submit">
+                    Send Message
+                </button>
+                {/* <input type="submit" className="field submit-button" value="Send Message" /> */}
+            </form>
+            {/* 
+            <% if @show_checkbox_recaptcha %>
+                <%= recaptcha_tags noscript: false %>
+            <% else %>
+                <%= recaptcha_v3(action: 'contact', site_key: ENV['RECAPTCHA_SITE_KEY_V3']) %>
+            <% end %>    
+            */}
+        </>
     );
 }
 
