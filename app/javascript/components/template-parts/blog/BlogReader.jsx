689b3f8cab9d150078c7621f88860a0acd99e7ac
Christopher Centrella <ccentrella.j@gmail.com>
2022-11-21 21:40:17 -0500

    Add subscribe block

--- app/javascript/components/template-parts/blog/BlogReader.jsx
+++ app/javascript/components/template-parts/blog/BlogReader.jsx
@@ -1,32 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Subscribe from '../Subscribe';
 
 function BlogReader() {
     const [loaded, setLoaded] = useState(false);
     const [post, setPost] = useState();
     const params = useParams();
-    const parse = require("html-react-parser");
+    const parse = require('html-react-parser');
 
     useEffect(() => {
-        document.title = "View Blog Post | Chris Centrella";
+        document.title = 'View Blog Post | Chris Centrella';
     }, []);
 
     useEffect(() => {
-        fetch("/api/v1/blog/" + params.slug)
+        fetch('/api/v1/blog/' + params.slug)
             .then((response) => response.json())
             .then((post) => {
                 console.log(post);
                 setPost(post);
-                document.title = post.title + " | Chris Centrella";
+                document.title = post.title + ' | Chris Centrella';
             });
-            setLoaded(true);
-            
+        setLoaded(true);
     }, [loaded]);
 
     if (!loaded) {
-        return <></>
-    }
-    else if (!post) {
+        return <></>;
+    } else if (!post) {
         document.title = "Page Doesn't Exist | Chris Centrella";
         return (
             <div className="container">
@@ -43,40 +42,46 @@ function BlogReader() {
         );
     }
     return (
-        <article className="post">
-            <section className="post-header">
-                <div className="post-header-top">
-                    <Link className="posts-back-icon" to="/blog" title="View All Articles">
-                        <i className="icon left-arrow medium"></i>
-                    </Link>
-                    {/* <% if user_signed_in? %>
+        <>
+            <article className="post">
+                <section className="post-header">
+                    <div className="post-header-top">
+                        <Link
+                            className="posts-back-icon"
+                            to="/blog"
+                            title="View All Articles">
+                            <i className="icon left-arrow medium"></i>
+                        </Link>
+                        {/* <% if user_signed_in? %>
                 <%= link_to "<i className='icon pencil large'></i>".html_safe, edit_post_path(@post), class: "post-edit-icon", title: "Edit Post" %>
             <% end %> */}
-                    <h1>{post.title}</h1>
-                </div>
-                <div className="post-header-bottom">
-                    <div className="post-details">
-                        <p>
-                            <i className="calendar large"></i> {post.date}{" "}
-                        </p>
-                        <p>
-                            <i className="clock large"></i> {post.time}{" "}
-                        </p>
+                        <h1>{post.title}</h1>
                     </div>
-                </div>
-                <div className="container">
-                    <div className="center">
-                        <p className="post-author button-label">
-                            <i className="person large"></i> {post.author}{" "}
-                        </p>
+                    <div className="post-header-bottom">
+                        <div className="post-details">
+                            <p>
+                                <i className="calendar large"></i> {post.date}{' '}
+                            </p>
+                            <p>
+                                <i className="clock large"></i> {post.time}{' '}
+                            </p>
+                        </div>
                     </div>
-                    <p className="post-description">{post.description}</p>
-                </div>
-            </section>
-            <section className="post-content container">
-                {post.content.body && parse(post.content.body)}
-            </section>
-        </article>
+                    <div className="container">
+                        <div className="center">
+                            <p className="post-author button-label">
+                                <i className="person large"></i> {post.author}{' '}
+                            </p>
+                        </div>
+                        <p className="post-description">{post.description}</p>
+                    </div>
+                </section>
+                <section className="post-content container">
+                    {post.content.body && parse(post.content.body)}
+                </section>
+            </article>
+            <Subscribe />
+        </>
     );
 }
 
