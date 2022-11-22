e42aa23507be3759f26c470c1cd4ea17ec04bda1
Christopher Centrella <ccentrella.j@gmail.com>
2022-11-21 21:40:38 -0500

    Add intro text and subscribe block

--- app/javascript/components/pages/BlogHome.jsx
+++ app/javascript/components/pages/BlogHome.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import { useState, useEffect } from "react";
-
-import BlogEntry from "../template-parts/blog/BlogEntry";
+import { useState, useEffect } from 'react';
+import Subscribe from '../template-parts/Subscribe';
+import BlogEntry from '../template-parts/blog/BlogEntry';
 
 function BlogHome() {
     const [loaded, setLoaded] = useState(false);
@@ -13,25 +13,37 @@ function BlogHome() {
     }, []);
 
     useEffect(() => {
-        fetch("/api/v1/blog/")
+        fetch('/api/v1/blog/')
             .then((response) => response.json())
-            .then(
-                (posts) => {
-                    setPosts(posts);
-                    setLoaded(true);
-                },
-            );
+            .then((posts) => {
+                setPosts(posts);
+                setLoaded(true);
+            });
     }, [loaded]);
 
     return (
         <>
             <main className="container">
-                <h1>Articles</h1>
-                {Array.isArray(posts) && posts.map((post) => (
-                    <BlogEntry post={post} key={post.slug} />
-                    ))}
+                <h1>Blog</h1>
+                <p>
+                    I'm a Christian who believes that Jesus wants each of us to have an
+                    intimate relationship with Him, and that all might read the Word of
+                    God "as one having authority and not as the scribes." (Matthew
+                    7:28-9). Rather than repeating what is currenly popular in
+                    Christianity or the Church, I'm absolutely convinced that Jesus gave
+                    us His Word so that each of us might not only apply it to our lives
+                    but comprehend amid our own limitations the mysteries of God, which He
+                    has revealed to the little (Matthew 11:25-6).
+                </p>
+                <p>
+                    I write for this purpose: That all men and women might see the mighty
+                    works of God in our time through the eyes of faith, and that one day
+                    everyone will experience the glory of God, alive in the Church!
+                </p>
+                {Array.isArray(posts) &&
+                    posts.map((post) => <BlogEntry post={post} key={post.slug} />)}
             </main>
-            {/* <Subscribe /> */}
+            <Subscribe />
         </>
     );
 }
