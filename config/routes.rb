2c36b7ce5ccc61c86be2c3a321689135a4665549
Christopher Centrella <ccentrella.j@gmail.com>
2022-11-24 15:54:33 -0500

    Refactor routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   get '/highlights', to: 'pages#home'
   get '/contact', to: 'pages#home'
   get '/blog', to: 'pages#home'
+  get '/blog/:slug', to: 'pages#home'
+  get '/subscribers/:id/edit', to: 'pages#home'
 
+  # Authentication routes
   devise_for :users, skip: [:registrations], controllers: {
     sessions: 'users/sessions',
     unlocks: 'users/unlocks',
@@ -15,15 +18,11 @@ Rails.application.routes.draw do
     confirmations: 'users/confirmations',
     # omniauth_callbacks: 'users/omniauth_callbacks',
   }
-
   devise_scope :user do
     get '/profile', to: "users/admin#home", as: :profile
     get '/users/edit', to: "users/registrations#edit", as: :edit_user_registration
     patch '/users/id', to: "users/registrations#update", as: :user_registration
   end
-  # Define the root path route ("/")
-  # root "static_pages#home"
-  get '/feed', to: "posts#index", format: 'rss'
 
   # Define external URLs
   direct :github do
@@ -38,13 +37,17 @@ Rails.application.routes.draw do
     "https://lightroom.adobe.com/gallery/35c62e7ff3ce476f9ae5b07994eb6f15"
   end
 
+  # Define routes for PDF resources
   resources :resources, path: '/resources'
   
+  # Define routes using old post API.
+  # This is a temporary solution to administer the admin backend until we convert everything to React
   resources :posts, path: '/blog', only: [:new, :create, :edit, :update, :destroy], param: :slug do
     get '/index/admin', on: :collection, as: 'admin_index', to: "posts#admin_index"
-    get '/feed', on: :collection, to: "posts#index", format: 'rss'
   end 
 
+  # Define routes using new APIs
+  get '/feed', to: "api/v1/posts#index", format: 'rss'
   namespace :api do
     namespace :v1 do
       resources :posts, path: '/blog', only: [:index, :show], param: :slug do
@@ -54,10 +57,9 @@ Rails.application.routes.draw do
       resources :subscribers, only: [:create, :edit, :destroy]
     end
   end
-  get '/blog/:slug', to: 'pages#home'
-  get '/subscribers/:id/edit', to: 'pages#home'
+  get '/blog/feed', to: "api/v1/posts#index", format: 'rss'
 
-  # Define error path
+  # Define error paths
   match '/404', via: :all, to: 'errors#not_found'
   match '/500', via: :all, to: 'errors#server_error'
 end
