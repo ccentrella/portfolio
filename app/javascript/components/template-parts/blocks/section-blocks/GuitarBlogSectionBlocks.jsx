99e3475e7d8d384cf059b5992591ce81d02b8aad
Christopher Centrella <ccentrella.j@gmail.com>
2022-11-25 13:10:29 -0500

    Update blog block text

--- app/javascript/components/template-parts/blocks/section-blocks/GuitarBlogSectionBlocks.jsx
+++ app/javascript/components/template-parts/blocks/section-blocks/GuitarBlogSectionBlocks.jsx
@@ -9,7 +9,21 @@ function GuitarBlogSectionBlocks({ src, alt, color }) {
             </div>
             <div className="section-block-content backdrop flex-col">
                 <h2 className="button-label">Personal Blog</h2>
-                <p>My faith life is important to me and I love to write about how to find God amid the twists and turns of life, and so others might know more about Him.</p>
+                <p>
+                    I'm a Christian who believes that Jesus wants each of us to have an
+                    intimate relationship with Him, and that all might read the Word of
+                    God "as one having authority and not as the scribes." (Matthew
+                    7:28-9). Rather than repeating what is currently popular in
+                    Christianity or the Church, I'm absolutely convinced that Jesus gave
+                    us His Word so that each of us might not only apply it to our lives
+                    but comprehend amid our own limitations the mysteries of God, which He
+                    has revealed to the little (Matthew 11:25-6).
+                </p>
+                <p>
+                    I write for this purpose: That all men and women might see the mighty
+                    works of God in our time through the eyes of faith, and that one day
+                    everyone will experience the glory of God, alive in the Church!
+                </p>
             </div>
         </div>
     );
