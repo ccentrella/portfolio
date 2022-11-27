6adc13d22bbb6b3e2068f6c023a228bcb4c04aa9
Christopher Centrella <ccentrella.j@gmail.com>
2022-11-26 22:25:39 -0500

    Documentation: Add JSDoc comment to ScrollToTop component

--- app/javascript/components/ScrollToTop.jsx
+++ app/javascript/components/ScrollToTop.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+/**
+ * Scroll to top once user navigates to new page
+ * @returns 
+ */
 function ScrollToTop() {
     const { pathname } = useLocation();
     useEffect(() => {
         window.scrollTo(0, 0);
-        console.log(pathname);
     }, [pathname]);
 
     return null;
