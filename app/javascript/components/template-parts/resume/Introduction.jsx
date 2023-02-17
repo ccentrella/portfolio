31db399315cf3bbe55777a9535703d42b52136e8
Christopher Centrella <ccentrella.j@gmail.com>
2023-02-17 01:47:30 -0600

    Update resume

--- app/javascript/components/template-parts/resume/Introduction.jsx
+++ app/javascript/components/template-parts/resume/Introduction.jsx
@@ -9,13 +9,13 @@ function Introduction() {
             <Hero />
             <p className="section-title zero-margin">Full Stack Software Engineer</p>
             <div className="container">
-                <p className="resume-location">Location: Lansing, MI</p>
+                <p className="resume-location">Lives In: Nashville, TN</p>
                 <div className="resume-buttons">
                     <Link className="button-fixed" to="/">
                         Homepage
                     </Link>
                     <a className="button-fixed" target="_blank" href="https://github.com/ccentrella">
-                        View GitHub
+                        Find Me on GitHub
                     </a>
                     <a className="button-fixed" target="_blank" href="https://linkedin.com/in/ccentrella">
                         View LinkedIn
--- app/javascript/components/template-parts/resume/WorkExperience.jsx
+++ app/javascript/components/template-parts/resume/WorkExperience.jsx
@@ -5,18 +5,28 @@ function WorkExperience() {
         <section>
             <p className="section-title">Work Experience</p>
             <div className="container">
-                <div>
-                    <p className="resume-item-heading">
-                        Association Application Developer
+            <div>
+                    <p className="resume-item-heading">Software Engineer II</p>
+                    <p className="resume-item-subheading">
+                        Groups360, Nashville (TN)
                         <br />
-                        IBM Consulting, Lansing (MI)
+                        Jan 2023 — Present
                     </p>
-                    <p className="resume-item-subheading">Jun 2022 — Present</p>
+                    <p>Completed code analysis using Sonar and documented recurring issues</p>
+                    <p>Technologies Used: Ruby, React, .NET, SonarQube</p>
+                </div>
+                <div>
+                    <p className="resume-item-heading">Software Developer</p>
+                    <p className="resume-item-subheading">
+                        IBM Consulting, Lansing (MI)
+                        <br />
+                        Jun 2022 — December 2022
+                    </p>
                     <p>Completed 350+ hours of training for several technologies - Java, Spring, Spring Boot, UML, Git, Agile, DevOps, Design Thinking</p>
                     <p>Earned 7 badges, covering software engineering practices like Agile and Design Thinking</p>
                     <p>Earned AWS Practitioner Certificate</p>
                     <p>Completed the HR case study - web project for the HRMS with 10+ features, applying all the tech and skills learned and exceeded expectations by making a very refined user experience</p>
-                    <p>Currently assigned to work for a large international client in a virtual team and report directly to the Project Manager, primarily working with Oracle PL/SQL</p>
+                    <p>Worked for a large international client in a virtual team and report directly to the Project Manager, primarily working with Oracle PL/SQL</p>
                 </div>
                 <div>
                     <p className="resume-item-heading">Junior Software Engineer</p>
