74a42976fc3288e0249fb202d22d07e78eec6dec
Christopher Centrella <ccentrella.j@gmail.com>
2023-03-10 19:27:39 -0600

    Update hero and resume text

--- app/javascript/components/template-parts/Hero.jsx
+++ app/javascript/components/template-parts/Hero.jsx
@@ -19,8 +19,8 @@ function Hero() {
                     </div>
                 </div>
                 <div id="hero-about">
-                    <p>I'm a capable Software Engineer II committed to creating a quality product, continuous improvement, and effective team collaboration.</p>
-                    <p>Writing my first line of code when I was fourteen, I've created numerous solutions across domains, including iOS mobile app development using Apple's latest APIs, and full stack web development using React with Ruby on Rails.</p>
+                    <p>I'm a capable Software Engineer II committed to creating a quality product, continuously improving my skills, and effectively collaborating on a team.</p>
+                    <p>Writing my first line of code when I was fourteen, I've created numerous solutions across domains, including iOS mobile app development using Apple's latest APIs, and full-stack web development using React with Ruby on Rails.</p>
                     <p>Most importantly, I'm committed to writing clean, quality code and following best software patterns and practices.</p>
                 </div>
             </div>
--- app/javascript/components/template-parts/resume/Summary.jsx
+++ app/javascript/components/template-parts/resume/Summary.jsx
@@ -6,7 +6,7 @@ function Summary() {
             <p className="section-title">Summary</p>
             <div className="container">
                 <p>Capable Software Engineer II committed to creating a quality product, continuous improvement, and effective team collaboration</p>
-                <p>Created numerous solutions across domains, including iOS mobile app development using Apple's latest APIs, and full stack web development using React with Ruby on Rails</p>
+                <p>Created numerous solutions across domains, including iOS mobile app development using Apple's latest APIs, and full-stack web development using React with Ruby on Rails</p>
                 <p>Committed to writing clean, quality code and following best software patterns and practices</p>
             </div>
         </section>
