c780e017c64739f0fc3eb2469fab144905ff0bbc
Christopher Centrella <ccentrella.j@gmail.com>
2023-06-04 00:05:58 -0500

    switch application to API mode

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.api_only = true
+
 module Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
