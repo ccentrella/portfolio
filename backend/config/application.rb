1d9a45a7306ba57aa110aa2e15b3acabca6516e2
Christopher Centrella <ccentrella.j@gmail.com>
2023-06-04 11:56:53 -0500

    simplify file; move api only mode to correct location

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,27 +1,26 @@
 require_relative "boot"
-
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.api_only = true
+# Base configuration goes here. Settings can be overridden in specific environments
+# using the files in config/environments, which are processed later.
 
 module Portfolio
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
+    config.api_only = true
     config.time_zone = 'Eastern Time (US & Canada)'
-    # config.eager_load_paths << Rails.root.join("extras")
 
     # Load images in emails
     config.action_mailer.asset_host = 'https://chriscentrella.com'
+
+    # Configure autoloading and eager loading
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
