8c81c369514751f4c060962f298815ed2d2e2c3f
Christopher Centrella <ccentrella.j@gmail.com>
2023-06-24 21:46:45 -0500

    update user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
 
+  has_secure_password
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
   def User.get_user_name(id)
     if User.exists?(id)
       return User.find(id).name
