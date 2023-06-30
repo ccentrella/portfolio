41535185069d4ec5fcae903282399875906fccca
Christopher Centrella <ccentrella.j@gmail.com>
2023-06-30 17:22:18 -0500

    remove reference to delete vendor directory

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets .scss
-//= link_tree ../../../vendor/javascript .js
 //= link_tree ../resources
 //= link_tree ../builds
