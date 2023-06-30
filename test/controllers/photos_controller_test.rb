291411216c85c931f586539b62f44c6990871d79
Christopher Centrella <ccentrella.j@gmail.com>
2023-06-30 17:13:22 -0500

    remove unused files and folders

--- test/controllers/photos_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class PhotosControllerTest < ActionDispatch::IntegrationTest
-  test "should get photos home" do
-    get photos_url
-    assert_response :success
-    assert_select "title", "Gallery | Chris Centrella"
-  end
-end
