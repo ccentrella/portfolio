486bc10f56bd3f6f768e19f4b3d50abb6d8d2c31
Christopher Centrella <ccentrella.j@gmail.com>
2023-07-01 15:04:33 -0500

    tweak styling and use section-title heading for uniformity

--- app/javascript/components/pages/BlogHome.jsx
+++ app/javascript/components/pages/BlogHome.jsx
@@ -23,23 +23,25 @@ function BlogHome() {
 
     return (
         <>
+            <p className="section-title thin">Blog</p>
             <main className="container">
-                <h1>Blog</h1>
-                <p>
-                    I'm a Christian who believes that Jesus wants each of us to have an
-                    intimate relationship with Him, and that all might read the Word of
-                    God "as one having authority and not as the scribes." (Matthew
-                    7:28-9). Rather than repeating what is currently popular in
-                    Christianity or the Church, I'm absolutely convinced that Jesus gave
-                    us His Word so that each of us might not only apply it to our lives
-                    but comprehend amid our own limitations the mysteries of God, which He
-                    has revealed to the little (Matthew 11:25-6).
-                </p>
-                <p>
-                    I write for this purpose: That all men and women might see the mighty
-                    works of God in our time through the eyes of faith, and that one day
-                    everyone will experience the glory of God, alive in the Church!
-                </p>
+                <div className='blog-description'>
+                    <p>
+                        I'm a Christian who believes that Jesus wants each of us to have an
+                        intimate relationship with Him, and that all might read the Word of
+                        God "as one having authority and not as the scribes." (Matthew
+                        7:28-9). Rather than repeating what is currently popular in
+                        Christianity or the Church, I'm absolutely convinced that Jesus gave
+                        us His Word so that each of us might not only apply it to our lives
+                        but comprehend amid our own limitations the mysteries of God, which He
+                        has revealed to the little (Matthew 11:25-6).
+                    </p>
+                    <p>
+                        I write for this purpose: That all men and women might see the mighty
+                        works of God in our time through the eyes of faith, and that one day
+                        everyone will experience the glory of God, alive in the Church!
+                    </p>
+                </div>
                 {Array.isArray(posts) &&
                     posts.map((post) => <BlogEntry post={post} key={post.slug} />)}
             </main>
