6f852e8451a6545229957eef4c23f5c4d0e87cf6
Christopher Centrella <ccentrella.j@gmail.com>
2023-07-04 12:17:11 -0500

    create nextjs skeleton project

--- frontend/javascript/assets.js
+++
@@ -1,7 +0,0 @@
-export async function asset(asset_name) {
-    const path = `/asset/${asset_name}/`;
-    const response = await fetch(path);
-    const data = await response.text();
-    
-    return data;
-}
